87dc06cdcd6582236a24724f380fad709cafcfb3
miguel <devnull@localhost>
2006-10-14 02:00:21 +0100

    [svn] timer is now portable (linux or win32)

--- include/timer.h
+++ include/timer.h
@@ -1,7 +1,31 @@
 /* timer.h 
    simple cpu timer api.
    */
+#ifdef __linux__
+#include <sys/time.h>
+#include <time.h>
+#endif
+
+#ifdef WIN32
+#include <sys/types.h> 
+#include <sys/timeb.h> 
+#include <winsock2.h> //timeval
+#endif
+
+#define my_time			timeval
+double getTimeDiff(struct my_time *start, struct my_time *stop);
+int getCurrTime(struct my_time *tv);
 
 
-double getElapsedTime(struct timeval *start, struct timeval *stop);
-int getCurrTime(struct timeval *tv);
+#ifdef WIN32
+/* define gettimeofday() */
+static inline int gettimeofday(struct timeval *tv, void *ignore) 
+{
+	struct _timeb buf;
+	_ftime_s(&buf);
+	tv->tv_sec = buf.time;
+	tv->tv_usec = buf.millitm * 1000;
+	return 0;
+}
+#endif
+
--- src/timer.c
+++ src/timer.c
@@ -1,25 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <time.h>
-#include "debug.h"
 #include "timer.h"
 
-double getElapsedTime(struct timeval *start, struct timeval *stop)
+double getElapsedTime(struct my_time *start, struct my_time *stop)
 {
 	double d = stop->tv_sec - start->tv_sec;
 	double c = stop->tv_usec - start->tv_usec;
 	if(c < 0) {
-		printf_dbg2("microsecs has wrapped around\n");
 		d--;
-		c =  start->tv_usec - stop->tv_usec;
+		c = 1000000 - start->tv_usec + stop->tv_usec;
 	}
 	c /= 1000000;
 	return  d + c;
 }
 	
-int getCurrTime(struct timeval *tv)
+int getCurrTime(struct my_time *tv)
 {
     return gettimeofday(tv,NULL);
 }
-
