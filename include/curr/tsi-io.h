a12f75c9f453a0cedcac0eb6af8e615be9d8825f
miguel <miguel@feynman.ist.utl.pt>
2007-06-22 19:46:38 +0100

    remove unused old include files. remove execute permissions from source files

--- include/curr/tsi-io.h
+++
@@ -1,9 +0,0 @@
-int readasciiXYZ(char *, float *, int , int , int );
-int readascii(char *, float *, unsigned int);
-int readbinary(char *, float *, long);
-int writeasciiXYZ(char *, float *, int , int , int ); 
-int writeascii(char *, float *, unsigned int);
-int writebinary(char *, float *, long);
-
-
-
