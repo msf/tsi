6cbf710c869c422828a65b5b47ba85671e18886e
Miguel Sousa Filipe <miguel.filipe@gmail.com>
2009-05-30 16:30:06 +0100

    Create new thread-safe RNG API:     genrand_*() Make current RNG thread-safe, requires using NEW API. Old API:     tsi_random_*() still thread unsafe.
    TESTS: add tests to the RNG.

--- test/test_coords.c
+++ test/test_coords.c
@@ -9,6 +9,40 @@
 #define NY 4
 #define NZ 5
 
+
+int test_origin_coords(int nxy, int nx)
+{
+    int i, j;
+
+    // dss origin coord is 1x1x1, tsi origin coord is 0x0x0 
+    // So, we should allways have a +1 offset in each coordinate
+    // in dss, grid starts at 1, up to MAX_LEN
+    // in tsi, grid starts at 0, up to MAX_LEN-1
+    i = get_grid_point( 0, 0, 0, nx, nxy);
+    assert(i == 0);
+
+    j = getPos( 1, 1, 1, nx, nxy);
+    assert(j == 1);
+
+    return 0;
+}
+
+int test_max_coords(int nx, int ny, int nz)
+{
+    int nxy = nx * ny;
+    int max = nxy * nz;
+    int i, j;
+
+    j = get_grid_point(nx-1, ny-1, nz-1, nx, nxy);
+    assert(j == max -1);
+
+    j = getPos(nx, ny, nz, nx, nxy);
+    assert(j ==  max);
+
+    return 0;
+}
+
+
 int main(int arvc, char *argv[])
 {
     int nxy = NX * NY;
@@ -18,10 +52,10 @@ int main(int arvc, char *argv[])
     int max = NX * NY * NZ;
     int min = 0;
 
-    // dss origin coord is 1x1x1, tsi origin coord is 0x0x0 
-    // So, we should allways have a +1 offset in each coordinate
-    // in dss, grid starts at 1, up to MAX_LEN
-    // in tsi, grid starts at 0, up to MAX_LEN-1
+    test_origin_coords(nxy, NX);
+    test_max_coords(NX, NY, NZ);
+    test_ilegal_coords(NX, NY, NZ);
+
     for(x = 0; x < NX; x++)
         for(y = 0; y < NY; y++)
             for(z = 0; z < NZ; z++) {
@@ -37,11 +71,6 @@ int main(int arvc, char *argv[])
                 assert(j+1 == t);
             }
 
-    j = get_grid_point(NX-1, NY-1, NZ-1, NX, nxy);
-    assert(j == max -1);
-
-    j = getPos(NX, NY, NZ, NX, nxy);
-    assert(j ==  max);
 
     return 0;
 }
---
+++ test/test_rand.c
@@ -0,0 +1,139 @@
+#include <assert.h>
+
+#include "math_random.h"
+
+typedef unsigned long ulong_t;
+
+ulong_t get_ulong(long seed)
+{
+    genrand_t *g = genrand_new(seed);
+    ulong_t l = genrand_ulong(g);
+    genrand_free(g);
+    return l;
+}
+
+double get_real(long seed)
+{
+    genrand_t *g = genrand_new(seed);
+    double l = genrand_real(g);
+    genrand_free(g);
+    return l;
+}
+
+
+void test_mt_long(long seed)
+{
+    ulong_t l1, l2;
+
+    l1 = get_ulong(seed);
+    l2 = get_ulong(seed);
+
+    assert(l1 == l2);
+}
+
+void test_mt_real(long seed)
+{
+    double l1, l2;
+
+    l1 = get_real(seed);
+    l2 = get_real(seed);
+
+    assert(l1 == l2);
+}
+
+void test_static_long(long seed)
+{
+    ulong_t l1, l2;
+
+    tsi_seed_random(seed);
+
+    l1 = get_ulong( seed );
+    l2 = tsi_random();
+
+    assert(l1 == l2);
+}
+
+void test_static_real(long seed)
+{
+    double l1, l2;
+
+    tsi_seed_random(seed);
+
+    l1 = get_real(seed);
+    l2 = tsi_random_real();
+
+    assert(l1 == l2);
+}
+
+void test_many_long(long seed, int count)
+{
+    ulong_t l1, l2;
+    int i;
+    genrand_t * g = genrand_new(seed);
+
+    tsi_seed_random(seed);
+
+    for(i = 0; i < count; i++) {
+        l1 = genrand_ulong(g);
+        l2 = tsi_random();
+        assert(l1 == l2);
+    }
+    genrand_free(g);
+}
+
+void test_many_real(long seed, int count)
+{
+    double l1, l2;
+    int i;
+    genrand_t * g = genrand_new(seed);
+
+    tsi_seed_random(seed);
+
+    for(i = 0; i < count; i++) {
+        l1 = genrand_real(g);
+        l2 = tsi_random_real();
+        assert(l1 == l2);
+    }
+    genrand_free(g);
+}
+
+
+void test_noneq_long(long seed)
+{
+    ulong_t l1, l2;
+
+    l1 = get_ulong(seed);
+    l2 = get_ulong(seed-3);
+
+    assert(l1 != l2);
+}
+
+void test_noneq_real(long seed)
+{
+    double l1, l2;
+
+    l1 = get_real(seed);
+    l2 = get_real(seed-3);
+
+    assert(l1 != l2);
+}
+
+
+int main(int argc, char *argv[])
+{
+
+    long a[] = { 0, -1, 9, 32766, 5794, 3883, 30914, 3159 };
+    int i;
+
+    for(i =0; i < sizeof(a); i++) {
+        test_static_long( a[i] );
+        test_static_real( a[i] );
+        test_mt_long( a[i] );
+        test_mt_real( a[i] );
+        test_noneq_long( a[i] );
+        test_noneq_real( a[i] );
+    }
+    return 0;
+}
+
+
